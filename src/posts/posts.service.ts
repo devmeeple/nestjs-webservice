05c409f8c67fc8468c68336a06726a206c096c0b
미플 <devmeeple@gmail.com>
2023-11-21 18:04:23 +0900

    [WSOLO-9] 단건조회, 업데이트, 삭제 단위테스트
    - update-post.dto 추가

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {PostsEntity} from './entities/posts.entity';
 import {Repository} from 'typeorm';
 import {CreatePostDto} from './dto/create-post.dto';
+import {UpdatePostDto} from './dto/update-post.dto';
 
 @Injectable()
 export class PostsService {
@@ -23,5 +24,48 @@ export class PostsService {
         // 전체조회: find()
         // 단거조회: findOne()
         return await this.postsRepository.find();
+    }
+
+    async findById(id: number) {
+        const post = await this.postsRepository.findOne({
+            where: {
+                id,
+            },
+        });
+
+        if (!post) {
+            throw new NotFoundException('데이터를 찾을 수 없습니다.');
+        }
+
+        return post;
+    }
+
+    async delete(id: number) {
+        const foundPost = await this.postsRepository.findOne({
+            where: {
+                id,
+            },
+        });
+
+        if (!foundPost) {
+            throw new NotFoundException('데이터를 찾을 수 없습니다.');
+        }
+
+        return await this.postsRepository.delete(id);
+    }
+
+    async update(id: number, updateRequest: UpdatePostDto ) {
+        const foundPost = await this.postsRepository.findOne({
+            where: {
+                id,
+            }
+        });
+
+        if (!foundPost) {
+            throw new NotFoundException('데이터를 찾을 수 없습니다.');
+        }
+
+        Object.assign(foundPost, updateRequest);
+        return await this.postsRepository.save(foundPost);
     }
 }
